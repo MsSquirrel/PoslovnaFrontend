353f9e098629f8849ee9201296ed11d193015010
Kristina <kristinasatara@gmail.com>
2016-06-22 02:18:31 +0200

    business year for robne kartice

--- src/scripts/robne-karticeService.js
+++ src/scripts/robne-karticeService.js
@@ -11,8 +11,18 @@ module.exports = [
 			});
 		}
 
+		function get_all_robnaKartica()
+		{
+			var resUrl = "http://localhost:61769/api/robna_kartica";
+			return $http.get(resUrl)
+			.then(function(response) {
+				return response.data;
+			});
+		}
+
 		return{
-			get_filtered_robnaKartica: get_filtered_robnaKartica,	
+			get_filtered_robnaKartica: get_filtered_robnaKartica,
+			get_all_robnaKartica: get_all_robnaKartica, 	
 		};
 
 	}
